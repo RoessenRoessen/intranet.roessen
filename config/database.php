651062f24da854d7238025b291e1b7d56c37a359
RoessenRoessen <robert@roessen.nl>
2026-05-28 10:38:34 +0200

    port uit config verwijderd

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/env.php';
 try {
 
   $dsn = "mysql:host=" . DB_HOST .
-    ";port=" . DB_PORT .
+    //";port=" . DB_PORT .
     ";dbname=" . DB_NAME .
     ";charset=utf8mb4";
 
--- config/env.php
+++ config/env.php
@@ -13,7 +13,7 @@ if ($env === false) {
 }
 
 define('DB_HOST', $env['DB_HOST']);
-define('DB_PORT', $env['DB_PORT']);
+//define('DB_PORT', $env['DB_PORT']);
 define('DB_NAME', $env['DB_NAME']);
 define('DB_USER', $env['DB_USER']);
 define('DB_PASS', $env['DB_PASS']);
